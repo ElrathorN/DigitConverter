0f3106a9747e0f89c054247410bae27279563c8e
ElrathorN <xfron--@mailfence.com>
2020-11-23 23:25:54 +0100

    Initial commit

---
+++ ClientClass.java
@@ -0,0 +1,63 @@
+import letpackage.SolveBox;
+import java.util.*;
+
+public class ClientClass {
+    private static boolean exit = false;
+
+    public static void main(String[] args) {
+        startMenu();
+    }
+
+    public static void startMenu() {
+        int i, j, choice, decNumber;
+        int[] offsetNumber = new int[SolveBox.constant];
+        Scanner inputObj = new Scanner(System.in);
+        String stringOffsetNumber;
+
+        while(!exit) {
+            for(i = 0; i < SolveBox.constant * 2; i += 1)
+                System.out.print("=");
+            System.out.println("\n1.convert from dec to bin");
+            System.out.println("2.convert from bin to dec");
+            System.out.println("3.exit");
+
+            choice = inputObj.nextInt();
+            switch(choice) {
+                case 1:
+                    System.out.print("put your dec num ->");
+                    decNumber = inputObj.nextInt();
+                    offsetNumber = SolveBox.decToBin(decNumber);
+                    System.out.print(decNumber+" = ");
+                    for(i = 0; i < offsetNumber.length; i += 1)
+                        System.out.print(offsetNumber[i]);
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.print("put your bin num ->");
+                    inputObj.nextLine();
+                    stringOffsetNumber = inputObj.nextLine();
+                    System.out.print(stringOffsetNumber+" = ");
+                    offsetNumber = fromStringToIntArr(stringOffsetNumber);
+                    decNumber = SolveBox.binToDec(offsetNumber);
+                    System.out.println(decNumber);
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default :
+                    continue;
+            }
+        }
+    }
+
+    public static int[] fromStringToIntArr(String stringDigits) {
+        int[] intDigits = new int[stringDigits.length()];
+        String[] tmpBuffer = stringDigits.split("");
+
+        for(int i = 0; i < stringDigits.length(); i += 1)
+            intDigits[i] = Integer.parseInt(tmpBuffer[i]);
+
+        return intDigits;
+    }
+
+}
---
+++ letpackage/SolveBox.java
@@ -0,0 +1,65 @@
+package letpackage;
+import java.lang.Math.*;
+
+public class SolveBox {
+    public final static int constant = 16;
+    protected static int abstractOffset[] = new int[constant];
+
+    public static int[] decToBin(int ourDec) {
+
+        if(ourDec > 65535) {
+            System.out.println("your number bigger than 65535");
+            // 1111111111111111111 - is our offset and this number = 65535
+        }
+        int i = constant - 1;
+
+        do {
+            if((ourDec % 2) == 0) {
+                abstractOffset[i] = 0;
+            }
+            else if((ourDec % 2) != 0) {
+                abstractOffset[i] = 1;
+                if(ourDec == 1) {
+                    break;
+                }
+            }
+
+            ourDec = ourDec/2;
+            i -= 1;
+        } while(true);
+
+        return abstractOffset;
+    }
+
+    public static int binToDec(int[] ourBin) {
+        int offsetFlag = 0;
+        int digitCount = 0;
+        int i, j, decNumber = 0;
+        int startAt = 0;
+
+        if(ourBin.length >= constant) {
+            for(i = 0; i < constant; i++)
+                if(ourBin[i] == 1) offsetFlag += 1;
+        }
+        if(offsetFlag > 15)
+            System.out.println("your number bigger than our offset 65535");
+
+        for(i = 0; i < ourBin.length; i += 1) {
+            if(ourBin[i] == 1) {
+                startAt += i;
+                for(j = i; j < ourBin.length; j += 1) {
+                    digitCount += 1;  //to count bin digit in our offset
+                }
+                break;
+            }
+        }
+
+        j = digitCount-1;
+        for(i = startAt; i < startAt+digitCount; i += 1) {
+            decNumber = decNumber + (int)(ourBin[i] * (Math.pow(2, j)));
+            j -=  1;
+        }
+
+        return decNumber;
+    }
+}
